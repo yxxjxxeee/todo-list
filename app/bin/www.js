2ab873668324becb6807cba35fe72c84762fa564
YJE <c0d1ngyje@gmail.com>
2025-07-15 14:14:20 +0900

    폴더 위치 변경

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const app = require("../app");
-const logger = require("../src/config/logger");
+const app = require("../../app");
+const logger = require("../../src/config/logger");
 const PORT = process.env.PORT | 3000;
 
 app.listen(PORT, () => {
